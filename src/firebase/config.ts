e9c9230841cdc2ec85dcc5e125d1636cf29024a3
Skat Skadi <skatskadi17@gmail.com>
2025-10-27 00:50:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2840555334-43709",
+  "appId": "1:677234921275:web:20f3c36d1c68e93f64b11e",
+  "apiKey": "AIzaSyBahMh0pxV1-gqIFUzuRGiLLnusw0rQjHI",
+  "authDomain": "studio-2840555334-43709.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "677234921275"
+};
